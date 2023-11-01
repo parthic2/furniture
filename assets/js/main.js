608ae02d3b253462ac645cb62e2c3704fbfff243
parthi <Stackholic-Infotech@dev.azure.com>
2023-11-01 15:19:58 +0530

    1-11-23

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ window.addEventListener("load", function () {
                 headerSticky.classList.toggle("header-sticky", scroll >= 100);
             }
         });
-    }, 100);
+    }, 60);
 });
 
 // slider slider
